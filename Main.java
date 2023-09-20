607ab0a53fc115e7f0f02b8fe8a309d1a5a925bf
LAKSHAY GARG <43146157+lakshaygarg0007@users.noreply.github.com>
2023-09-20 16:54:07 +0530

    added executor pool for limiting threads

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MyClass {
     public static void main(String [] args) {
         System.out.println("Thread Name - " +  Thread.currentThread().getName());
@@ -13,6 +16,12 @@ public class MyClass {
             NumberPrinter np = new NumberPrinter(i);
             Thread th = new Thread(np);
             th.start();
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i = 1; i <= 100; i++) {
+            NumberPrinter np = new NumberPrinter(i);
+            executorService.submit(np);
         }
     }
 }
